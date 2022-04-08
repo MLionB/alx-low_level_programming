cbff01251f3efd76843c889c12e0f87a2e35876a
root <nkbmeaza@gmail.com>
2022-04-08 11:04:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
@@ -9,8 +11,12 @@
  *    * Return: Always 0 (Success)
  *
  *     */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+			puts("\"Programming is like building a multilingual puzzle");
+						return (0);
+
 }
